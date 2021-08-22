cc654209ee24168a682c22b9d8fbdc88bfcad792
kianpas <kianpas@gmail.com>
2021-08-22 23:35:07 +0900

    2021.08.22 무중단 배포 수정4 커밋

--- scripts/profile.sh
+++ scripts/profile.sh
@@ -4,18 +4,14 @@
 
 function find_idle_profile() {
     RESPONSE_CODE=$(curl -s -o /dev/null -w "%{http_code}" http://localhost/profile)
-
-    if[ ${RESPONSE_CODE} -ge 400 ] #400보다 크면(40x, 50x 에러 포함)
-
-    then
+    #400보다 크면(40x, 50x 에러 포함)
+    if[ ${RESPONSE_CODE} -ge 400 ] then
       CURRENT_PROFILE=real2
     else
       CURRENT_PROFILE=$(curl -s http://localhost/profile)
     fi
-    
-    if[ ${RESPONSE_CODE} == real1 ] #400보다 크면(40x, 50x 에러 포함)
 
-    then
+    if[ ${RESPONSE_CODE} == real1 ] then
       IDLE_PROFILE=real2
     else
       IDLE_PROFILE=real1
@@ -28,9 +24,7 @@ function find_idle_profile() {
 function find_idle_port() {
     IDLE_PROFILE=$(find_idle_profile)
 
-    if [ ${IDLE_PROFILE} == real1 ]
-
-    then
+    if [ ${IDLE_PROFILE} == real1 ] then
       echo "8081"
     else
       echo "8082"
