8e8a4800924a44a811578c477ead0c16480ff65e
kianpas <kianpas@gmail.com>
2021-08-22 22:59:37 +0900

    2021.08.22 무중단 배포 수정2 커밋

--- scripts/profile.sh
+++ scripts/profile.sh
@@ -6,7 +6,7 @@ function find_idle_profile() {
     RESPONSE_CODE=$(curl -s -o /dev/null -w "%{http_code}" http://localhost/profile)
 
     if[ ${RESPONSE_CODE} -ge 400 ] #400보다 크면(40x, 50x 에러 포함)
-    then
+then
       CURRENT_PROFILE=real2
     else
       CURRENT_PROFILE=$(curl -s http://localhost/profile)
